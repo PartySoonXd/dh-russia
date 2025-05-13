f6229716cb5cdc09002e18ef064ae23684e5b825
PartySoonXd <vladbelom2005@gmail.com>
2025-05-13 23:52:56 +0700

    add QueryClientProvider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,17 +6,22 @@ import { ThemeProvider } from '@app/providers/ThemeProvider'
 import './shared/config/i18n/i18n'
 import { ErrorBoundary } from '@app/providers/ErrorBoundary'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const domNode = document.getElementById('root')
+const queryClient = new QueryClient()
+
 if (domNode) {
 	const root = createRoot(domNode)
 	root.render(
-		<BrowserRouter>
-			<ErrorBoundary>
-				<ThemeProvider>
-					<App />
-				</ThemeProvider>
-			</ErrorBoundary>
-		</BrowserRouter>,
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<ErrorBoundary>
+					<ThemeProvider>
+						<App />
+					</ThemeProvider>
+				</ErrorBoundary>
+			</BrowserRouter>
+		</QueryClientProvider>,
 	)
 }
