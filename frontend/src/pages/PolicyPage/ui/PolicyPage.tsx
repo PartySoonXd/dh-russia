f1524c912f476785097d22bf25a1e8feda3465aa
PartySoonXd <vladbelom2005@gmail.com>
2025-05-28 01:38:44 +0700

    test md

--- frontend/src/pages/PolicyPage/ui/PolicyPage.tsx
+++ frontend/src/pages/PolicyPage/ui/PolicyPage.tsx
@@ -1,5 +1,9 @@
+import { $publicApi } from '@shared/api/api'
 import { classNames } from '@shared/lib/classNames/classNames'
+import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import ReactMarkdown from 'react-markdown'
+import cls from './policy-page.module.scss'
 
 interface PolicyPageProps {
 	className?: string
@@ -7,12 +11,29 @@ interface PolicyPageProps {
 
 const PolicyPage = ({ className }: PolicyPageProps) => {
 	const { t } = useTranslation()
+	const [pageData, setPageData] = useState<string>()
 
-	return (
-		<div className={classNames('', {}, [className])}>
-			PolicyPage
-		</div>
-	)
+	useEffect(() => {
+		const getPageData = async (): Promise<void> => {
+			await $publicApi.get('/api/privacy-policy').then(({ data }) => {
+				setPageData(data.data.text as string)
+			}).catch((error) => {
+				console.log(error)
+			})
+		}
+
+		getPageData()
+	}, [])
+
+	if (pageData) {
+		return (
+			<div className={classNames('', {}, [className])}>
+				<div className={cls.md}>
+					<ReactMarkdown children={pageData} />
+				</div>
+			</div>
+		)
+	}
 }
 
 export default PolicyPage
